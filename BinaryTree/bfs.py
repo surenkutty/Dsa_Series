2ab934e65dda6f7972cad4f908772cb4fb6e3bc7
surenkutty <surenkutty2004@gmail.com>
2026-01-06 12:26:57 +0530

    bfs tree 50%

---
+++ BinaryTree/bfs.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, root, val):
+        if root is None:
+            return Node(val)
+
+        if val < root.val:
+            root.left = self.insert(root.left, val)
+        else:
+            root.right = self.insert(root.right, val)
+
+        return root
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            print(root.val, end=" ")
+            self.inorder(root.right)
+            
+    def preorder(self,root):
+        if root:
+            print(root.val,end=" ")
+            self.preorder(root.left)
+            self.preorder(root.right)
+    def postorder(self,root):
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            print(root.val,end=" ")
+
+    def search(self, root, key):
+        if root is None:
+            return False
+
+        if root.val == key:
+            return True
+        elif key < root.val:
+            return self.search(root.left, key)
+        else:
+            return self.search(root.right, key)
+    def height(self,root):
+        if root is None:
+            return 0
+        return 1+max(self.height(root.left),self.height(root.left))
+
+t=Tree()
+values = [10, 5, 15, 3, 7, 12]
+
+
+for v in values:
+    t.root = t.insert(t.root, v)
+
+print("Inorder traversal:")
+t.inorder(t.root)
+print("\npreorder traversal:")
+t.preorder(t.root)
+print("\npostorder traversal:")
+t.postorder(t.root)
+
+
+print("\nHeight of tree:", t.height(t.root))
