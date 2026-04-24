42dfe146d4bb49984608f2fbfc291c84cacc0399
Surend Kumar <105663626+surenkutty@users.noreply.github.com>
2026-04-24 04:36:33 +0000

    Add function to calculate maximum number of meetings in a given time interval

---
+++ greedyAlgorithms/meeting.py
@@ -0,0 +1,18 @@
+def meeting(A):
+    data=[]
+    
+    for s,e in A:
+        data.append((s,+1))
+        data.append((e,-1))
+    data.sort()
+    
+    curr=0
+    ans=0
+    
+    for _,v in data:
+        curr+=v
+        ans=max(ans,curr)
+    return ans
+        
+    
+print(meeting([[5,10],[15,20],[0,30]]))
